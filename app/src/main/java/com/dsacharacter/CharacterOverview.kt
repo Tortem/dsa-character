7900f53f4a4ed368b9efd142a7ac7592dbcba7d1
Torben <torbboettcher@outlook.de>
2023-12-16 23:54:52 +0100

    yo

---
+++ app/src/main/java/com/dsacharacter/CharacterOverview.kt
@@ -0,0 +1,31 @@
+package com.dsacharacter
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dsacharacter.theme.TestTheme
+
+@Composable
+fun CharacterOverview() {
+    Column() {
+        Text(
+            text = "Hello"
+        )
+        Text(
+            text = "kaljsdf"
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CharacterOverviewPreview() {
+    TestTheme {
+        CharacterOverview()
+    }
+}
