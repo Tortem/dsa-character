0283673c54e627d8e1c697db8b4c1f4588aa27eb
Olaf Kulczycki <olaf.kulczycki@gmail.com>
2023-12-17 01:22:11 +0100

    Added a Background

--- app/src/main/java/com/dsacharacter/CharacterOverview.kt
+++ app/src/main/java/com/dsacharacter/CharacterOverview.kt
@@ -1,11 +1,16 @@
 package com.dsacharacter
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dsacharacter.theme.TestTheme
 
@@ -24,8 +29,16 @@ fun CharacterOverview() {
 
 @Preview(showBackground = true)
 @Composable
-fun CharacterOverviewPreview() {
-    TestTheme {
-        CharacterOverview()
+fun Background() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.mittlerer_wertekasten_lq),
+            contentDescription = "defaultBackground",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+        TestTheme {
+            CharacterOverview()
+        }
     }
 }
