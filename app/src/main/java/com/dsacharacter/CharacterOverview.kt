5a363f9ad76e79a16d2513ad60ae576b825175c9
Torben <torbboettcher@outlook.de>
2023-12-17 01:55:55 +0100

    saldhfldsh

--- app/src/main/java/com/dsacharacter/CharacterOverview.kt
+++ app/src/main/java/com/dsacharacter/CharacterOverview.kt
@@ -1,33 +1,32 @@
 package com.dsacharacter
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.dsacharacter.theme.TestTheme
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CharacterOverview() {
-    Column() {
-        Text(
-            text = "Hello"
-        )
-        Text(
-            text = "kaljsdf"
-        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(painterResource(id = R.drawable.nameless), contentDescription = "", Modifier.align(
+            Alignment.TopEnd))
+        Column() {
+            Text("Edwin Talham", fontSize = 25.sp)
+            Text(text = "Hello")
+            Text(text = "@strings/")
+        }
     }
-
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun Background() {
     Box(modifier = Modifier.fillMaxSize()) {
@@ -37,8 +36,6 @@ fun Background() {
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.matchParentSize()
         )
-        TestTheme {
-            CharacterOverview()
-        }
+        CharacterOverview()
     }
 }
